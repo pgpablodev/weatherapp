878eff35c24ec8ac75fdc6fa931190558da91325
pgpablodev <povarg.pablo@gmail.com>
2022-10-29 17:58:34 +0200

    Working on mobile design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.scss';
+import { useEffect, useState } from 'react'
 import Layout from './components/Layout';
 
 function App() {
-  return (
-    <Layout />
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth)        
+    window.addEventListener("resize", handleResizeWindow)        
+    return () => {
+        window.removeEventListener("resize", handleResizeWindow)       
+    }
+  },[])
+
+  let cadena = `width=${width}, height=device-height, initial-scale=1`
+
+  return(
+    <div>
+      <meta name="viewport" content={cadena}></meta>
+      <Layout />
+    </div>    
   );
 }
 
